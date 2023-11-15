95bd865403eb140cec248505bd53e4da78d82873
Shimabukuro <s23012@std.it-college.ac.jp>
2023-11-15 09:35:55 +0900

    chapter 7-2 まで終わった。
    練習も終わってる

---
+++ src/exercise/E751_1.java
@@ -0,0 +1,18 @@
+package exercise;
+
+import javax.naming.InvalidNameException;
+import javax.naming.Name;
+import java.util.Enumeration;
+
+public class E751_1 {
+    public static void main(String[] args) {
+        Product p = new Product(890, 280, "ステンレスネジ");
+        Stock s890 = new Stock(p, 10);
+        System.out.println(s890);
+
+        p.setNumber(810);
+        s890.getProduct().setNumber(900);
+
+        System.out.println(s890);
+    }
+}
---
+++ src/exercise/Stock.java
@@ -0,0 +1,31 @@
+package exercise;
+
+public class Stock {
+    private final Product product;
+    private final int number;
+
+    public Product getProduct() {
+        return new Product(product.getNumber(), product.getPrice(), product.getName());
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    @Override
+    public String toString() {
+        return "Stock{" +
+                "product=" + product +
+                ", number=" + number +
+                '}';
+    }
+
+    public Stock(Product product, int number) {
+        /*
+        this.product = new Product(product.getNumber(), product.getPrice(), product.getName());*/
+        this.product = new Product(product);
+        this.number = number;
+
+
+    }
+}
