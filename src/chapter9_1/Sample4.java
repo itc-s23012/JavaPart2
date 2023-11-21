a406c2ef06c21ac8af94995273aa2c41a011365c
Shimabukuro <s23012@std.it-college.ac.jp>
2023-11-21 16:56:15 +0900

    chapter9 まで終わった。
    練習も終わってる

---
+++ src/chapter9_1/Sample4.java
@@ -0,0 +1,15 @@
+package chapter9_1;
+
+public class Sample4 {
+    public static void main(String[] args) {
+
+//        Member member = new GeneralMember(200, "木村一郎");
+//
+//        String msg = switch (m) {
+//            case GeneralMember gem -> "一般会員です";
+//            case StudentMember stm -> "学生会員です 期限日=" + stm.getExpDate();
+//            case SeniorMember sem -> "シニア会員です 年齢=" + sem.age();
+//            default -> "会員ではありません";
+//        };
+    }
+}
